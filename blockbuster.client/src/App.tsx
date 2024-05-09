13af3704359f72d27d901e0eb73b76e349d00ea3
Aucons94 <148054502+Aucons94@users.noreply.github.com>
2024-05-09 14:25:59 +0200

    Initial commit

---
+++ blockbuster.client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { BrowserRouter, Routes } from "react-router-dom";
+import NavigationBar from "./Components/NavigationBar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavigationBar />
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
